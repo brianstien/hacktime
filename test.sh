896169b3ba6954d043338276b3677182609a5224
brianstien <brianastien@gmail.com>
2016-02-11 20:25:59 -0700

    Add test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl http://localhost:3000/foos.json &
+curl http://localhost:3000/bars.json &
+curl http://localhost:3000/beers.json &
+curl http://localhost:3000/foos.json &
+curl http://localhost:3000/bars.json &
+curl http://localhost:3000/beers.json &
